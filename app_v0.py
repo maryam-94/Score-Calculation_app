bacafa9e2ed8ed3358e1769dac4b8eef089e8315
Maryam Chouchane <chouchane.maryam94@gmail.com>
2022-11-07 17:52:51 +0100

    chart data

--- app_v0.py
+++ app_v0.py
@@ -7,3 +7,8 @@ import altair as alt
 from datetime import datetime
 
 print('hello')
+chart_data = pd.DataFrame(
+    np.random.randn(50, 3),
+    columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
