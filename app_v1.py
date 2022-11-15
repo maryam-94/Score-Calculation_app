88799d1ecae7b76c6ef9d52966db6c07711d91d8
Maryam Chouchane <chouchane.maryam94@gmail.com>
2022-11-15 16:32:24 +0100

    update V1

--- app_v1.py
+++ app_v1.py
@@ -10,7 +10,8 @@ from datetime import datetime
 # DATA_URL = ('bureau.csv')
 @st.cache
 def load_data():
-    data = pd.read_pickle(r'C:\Users\chouc\openclassrooms\projet7\X_final1.pickle')
+    data = pd.read_pickle(r'C:\Users\chouc\openclassrooms\projet7\data_for_app.pickle')
+    data.reset_index(drop=True, inplace=True)
     # data['Date'] = pd.to_datetime(data['Date']).dt.strftime('%Y-%m-%d')
     return data
 df = load_data()
